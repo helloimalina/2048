7fd4b5ab89cf55f99555c0de74ce9c57d8760c23
helloimalina <72018395+helloimalina@users.noreply.github.com>
2020-10-23 16:11:19 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -58,6 +58,7 @@ class Game {
         this.restart();
 
         window.addEventListener('keyup', function(e) {
+            e.stopPropagation();
             switch (e.keyCode) {
                 case 38:
                     this.moveUp();
